938f4dc763919e7870201de74f6ff4340428556d
Lucas Alba <lchends@gmail.com>
2019-10-16 18:47:14 -0300

    Entrega ply (lexer/parser) pronta

--- src/ply/lexer_parser.py
+++ src/ply/lexer_parser.py
@@ -1,9 +1,10 @@
 # https://www.dabeaz.com/ply/example.html
 # http://www.dalkescientific.com/writings/NBN/parsing_with_ply.html
 
-# import lex
 import ply.lex as lex
 import ply.yacc as yacc
+
+'''Defining tokens'''
 
 tokens = (
     'INT',
@@ -27,10 +28,17 @@ tokens = (
     'ENDLOOP',
     'DO',
     'OR',
-    'AND'
+    'AND',
+    'IF',
+    'THEN',
+    'ELSE',
+    'ENDIF',
+    'NOT',
+    'INPUT'
 )
 
 
+'''Tokens regular expressions'''
 t_INT = r'\d+'
 t_PLUS = r'[+]'
 t_MINUS = r'[-]'
@@ -40,7 +48,7 @@ t_OPENPAR = r'\('
 t_CLOSEPAR = r'\)'
 t_IDENT = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-
+'''Functions for detecting strings'''
 def t_VARDEC(t):
     r'int |bool '
     return t
@@ -80,14 +88,31 @@ def t_DO(t):
 def t_OR(t):
     r'or '
     return t
-def t_AMD(t):
+def t_AND(t):
     r'and '
+    return t
+def t_IF(t):
+    r'if '
+    return t
+def t_THEN(t):
+    r'then'
+    return t
+def t_ELSE(t):
+    r'else'
+    return t
+def t_ENDIF(t):
+    r'endif'
+    return t
+def t_NOT(t):
+    r'not '
+    return t
+def t_INPUT(t):
+    r'input'
     return t
 
 
-
 t_ignore = " \t"
-# t_ignore = '\n+'
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count('\n')
@@ -100,37 +125,23 @@ def t_error(t):
 lexer = lex.lex()
 
 
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','MUL','DIV')
-)
-
-# variables = {}
-
-
-# def p_program(t):
-#     'program : stmts'
-#     t[0] = t[1]
-
 def p_stmts(t):
-    '''stmts : stmt
-             | stmts stmt'''
-    # print('stmts')
+    '''
+    stmts : stmt
+          | stmts stmt
+    '''
 
 def p_vardec(t):
     'stmt : VARDEC IDENT'
     print('vardec', t[1], t[2])
-    # variables[t[2]] = None
 
 def p_assig(t):
     'stmt : IDENT ASSIG relexpr'
     print('assig', t[1])
-    # variables[t[1]] = t[3]
 
 def p_print(t):
     'stmt : PRINT relexpr'
     print('print')
-    # variables[t[1]] = t[3]
 
 def p_funcdec(t):
     'stmt : VARDEC FUNC IDENT OPENPAR CLOSEPAR ASSIG stmts ENDFUNC'
@@ -140,41 +151,55 @@ def p_while(t):
     'stmt : WHILE relexpr DO stmts ENDLOOP'
     print('whileloop')
 
+def p_if(t):
+    '''stmt : IF relexpr THEN stmts ENDIF
+            | IF relexpr THEN stmts ELSE stmts ENDIF'''
+    print('if')
+
 def p_relexpr(t):
-    '''relexpr : expr
-               | expr GREATER expr
-               | expr LESS expr
-               | expr EQUALS expr'''
+    '''
+    relexpr : expr
+            | expr GREATER expr
+            | expr LESS expr
+            | expr EQUALS expr
+    '''
     print('relexpr')
 
-
-# def p_binop(t):
-#     '''expr : expr PLUS expr
-#             | expr MINUS expr
-#             | expr DIV expr
-#             | expr MUL expr'''
-
 def p_expr(t):
-    '''expr : term
-            | term PLUS term
-            | term MINUS term
-            | term OR term'''
+    '''
+    expr : term
+         | term PLUS term
+         | term MINUS term
+         | term OR term
+    '''
     print('expr')
 
 def p_term(t):
-    '''term : factor
-            | factor MUL factor
-            | factor DIV factor
-            | factor AND factor'''
+    '''
+    term : factor
+         | factor MUL factor
+         | factor DIV factor
+         | factor AND factor
+    '''
     print('term')
 
 def p_factor(t):
-    '''factor : INT
-              | IDENT
-              | BOOLVAL'''
+    '''
+    factor : INT
+           | IDENT
+           | BOOLVAL
+           | PLUS factor
+           | MINUS factor
+           | NOT factor
+           | OPENPAR relexpr CLOSEPAR
+           | INPUT OPENPAR CLOSEPAR
+    '''
 
 
 parser = yacc.yacc()
+
+
+'''Parser testing'''
 
 parser.parse(
      '''bool flag
@@ -184,14 +209,30 @@ parser.parse(
         x is 5
 
         int func Sum() is
-        print x
+            print x
         endfunc
         
         while x greater 0 do
-        print x
+            print x
         endloop
         
-        x is 5''')
+        x is 5
+        if flag then
+            print flag
+        endif
+        x is -x
+        int y
+        y is input()
+        flag is not flag
+        if 1 then
+            print 1
+        else
+            print 0
+        endif''')
+
+
+
+'''Lexer testing'''
 
 
 # lexer.input('''bool flag
